5f2725907a5d85da169957d24620ff650e5ff99b
Vi Tran <contact.vitran@gmail.com>
2021-12-08 14:57:59 +1100

    Finished assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    return (
+        <div>
+            <input type="password" onChange={() => console.log("Entering password...")}/>
+        </div>
+    );
+}
+
+export default Keypad;
